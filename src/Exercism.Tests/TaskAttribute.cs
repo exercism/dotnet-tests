6d3c8f75a00792335892d3359fab3194f18fdd58
Erik Schierboom <erik_schierboom@hotmail.com>
2021-05-06 11:12:31 +0200

    Rename task

---
+++ src/Exercism.Tests/TaskAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercism.Tests
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class TaskAttribute : Attribute
+    {
+        public int Number { get; }
+
+        public TaskAttribute(int number)
+        {
+            if (number <= 0) throw new ArgumentOutOfRangeException(nameof(number), number, "Number must be greater than zero");
+            
+            Number = number;
+        }
+    }
+}
