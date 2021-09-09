0290d36272b98e9eae133f293637c5b4a82feea5
Erik Schierboom <erik_schierboom@hotmail.com>
2021-09-09 13:04:50 +0200

    Add UseCultureAttribute

---
+++ src/Exercism.Tests/UseCultureAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace Exercism.Tests
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class UseCultureAttribute : BeforeAfterTestAttribute
+    {
+        private readonly CultureInfo _culture;
+        private readonly CultureInfo _uiCulture;
+
+        private CultureInfo _originalCulture;
+        private CultureInfo _originalUiCulture;
+
+        public UseCultureAttribute()
+        {
+            _culture = CultureInfo.InvariantCulture;
+            _uiCulture = CultureInfo.InvariantCulture;
+        }
+        
+        public UseCultureAttribute(string culture)
+            : this(culture, culture)
+        {
+        }
+
+        public UseCultureAttribute(string culture, string uiCulture)
+        {
+            _culture = new CultureInfo(culture, false);
+            _uiCulture = new CultureInfo(uiCulture, false);
+        }
+
+        public override void Before(MethodInfo methodUnderTest)
+        {
+            _originalCulture = CultureInfo.CurrentCulture;
+            _originalUiCulture = CultureInfo.CurrentUICulture;
+
+            CultureInfo.CurrentCulture = _culture;
+            CultureInfo.CurrentUICulture = _uiCulture;
+        }
+
+        public override void After(MethodInfo methodUnderTest)
+        {
+            CultureInfo.CurrentCulture = _originalCulture;
+            CultureInfo.CurrentUICulture = _originalUiCulture;
+        }
+    }
+}
---
+++ tests/Exercism.Tests.Tests/UseCultureAttributeTests.cs
@@ -0,0 +1,40 @@
+using System.Globalization;
+using Xunit;
+
+namespace Exercism.Tests.Tests
+{
+    [UseCulture("en-US")]
+    public class UseCultureAttributeTests
+    {
+        [Fact]
+        [UseCulture("nl-NL")]
+        public void SpecifyCulture()
+        {
+            Assert.Equal("nl-NL", CultureInfo.CurrentCulture.Name);
+            Assert.Equal("nl-NL", CultureInfo.CurrentUICulture.Name);
+        }
+        
+        [Fact]
+        [UseCulture("nl-NL", "nl-BE")]
+        public void SpecifyCultureAndUiCulture()
+        {
+            Assert.Equal("nl-NL", CultureInfo.CurrentCulture.Name);
+            Assert.Equal("nl-BE", CultureInfo.CurrentUICulture.Name);
+        }
+
+        [Fact]
+        [UseCulture]
+        public void DontSpecifyCulture()
+        {
+            Assert.Equal(CultureInfo.InvariantCulture, CultureInfo.CurrentCulture);
+            Assert.Equal(CultureInfo.InvariantCulture, CultureInfo.CurrentUICulture);
+        }
+        
+        [Fact]
+        public void InheritCultureFromClass()
+        {
+            Assert.Equal("en-US", CultureInfo.CurrentCulture.Name);
+            Assert.Equal("en-US", CultureInfo.CurrentUICulture.Name);
+        }
+    }
+}
